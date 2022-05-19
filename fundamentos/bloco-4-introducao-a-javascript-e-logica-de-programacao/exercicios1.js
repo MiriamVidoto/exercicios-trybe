d05507ba584e4d0c4f03f3232b47d87cdd0788db
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-18 23:39:26 -0300

    resolvendo primeira parte

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios1.js
@@ -0,0 +1,58 @@
+// Usando o objeto abaixo, faça os exercícios a seguir:
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+//   1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+// Bem-vinda, Margarida
+
+// console.log(`Bem-vinda ${info.personagem}`);
+
+// 🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+// {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   }
+
+info.recorrente = 'sim';
+// console.log(info);
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+// personagem
+// origem
+// nota
+// recorrente
+
+for ( let keys in info){
+    // console.log(keys);
+};
+
+// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+for ( let keys in info){
+    // console.log(info[keys]);
+};
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim',
+};
+
+const infos = `${info.personagem} e ${info2.personagem}
+${info.origem} e ${info2.origem}
+${info.nota} e ${info2.nota}
+Ambos recorrentes`
+
+console.log(infos);
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-function verificaPalindrome(string) {
-    let newString = string.reverse()
-    console.log(newString);
-}
-
-// Exemplo de palíndromo: arara. Retorno esperado: true
-console.log(verificaPalindrome('arara'));
